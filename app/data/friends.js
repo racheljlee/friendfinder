50b8baf6cb292057670e93876bebacc94e57d6ac
Ben Swolo <rachelkindacodes@gmail.com>
2018-05-24 23:35:23 -0500

    finished except for modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,23 +1,29 @@
 var friendsList = [
     {
-        name: "Sadeq",
-        photo: "https://emojipedia-us.s3.amazonaws.com/thumbs/320/google/6/winking-face_1f609.png",
-        scores: [1, 3, 4, 5, 4, 2, 3, 4, 2, 5]
-    },
-    {
-        name: "Halle",
-        photo: "https://emojipedia-us.s3.amazonaws.com/thumbs/320/google/6/smiling-face-with-open-mouth-and-smiling-eyes_1f604.png",
-        scores: [1, 2, 1, 4, 2, 2, 2, 5, 3, 5]
-    },
-    {
-        name: "Sabah",
-        photo: "https://emojipedia-us.s3.amazonaws.com/thumbs/320/google/6/smiling-face-with-sunglasses_1f60e.png",
-        scores: [3, 2, 5, 2, 1, 4, 4, 2, 1, 3]
+
+        name: "Rae",
+        photo: "https://i.imgur.com/A5ESVGN.png",
+        scores: [4, 2, 5, 2, 3, 4, 4, 1, 2, 2],
+        routeName: "rae"
+
     },
     {
         name: "Ali",
         photo: "https://emojipedia-us.s3.amazonaws.com/thumbs/320/google/6/face-with-tears-of-joy_1f602.png",
-        scores: [2, 5, 4, 3, 5, 1, 5, 2, 4, 3]
+        scores: [4, 3, 5, 1, 1, 2, 4, 3, 2, 3],
+        routeName: "ali"
+    },
+    {
+        name: "Sadeq",
+        photo: "https://emojipedia-us.s3.amazonaws.com/thumbs/320/google/6/winking-face_1f609.png",
+        scores: [1, 3, 4, 5, 4, 2, 3, 4, 2, 5],
+        routeName: "sadeq"
+    },
+    {
+        name: "Sabah",
+        photo: "https://emojipedia-us.s3.amazonaws.com/thumbs/320/google/6/smiling-face-with-sunglasses_1f60e.png",
+        scores: [3, 2, 5, 2, 1, 4, 4, 2, 1, 3],
+        routeName: "sabah"
     }
 ];
 
--- homework_instructions.md
+++ homework_instructions.md
@@ -37,13 +37,13 @@ You will use Express to handle routing. Make sure you deploy your app to Heroku 
 
 ✅ 3. Your `htmlRoutes.js` file should include two routes:
 
-   * A GET Route to `/survey` which should display the survey page.
-   * A default, catch-all route that leads to `home.html` which displays the home page. 
+   * A GET Route to `/survey` which should display the survey page. ✅
+   * A default, catch-all route that leads to `home.html` which displays the home page. ✅
 
-4. Your `apiRoutes.js` file should contain two routes:
+✅ 4. Your `apiRoutes.js` file should contain two routes:
 
-   * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
-   * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic. 
+   * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends. ✅
+   * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic. ✅
 
 5. You should save your application's data inside of `app/data/friends.js` as an array of objects. Each of these objects should roughly follow the format below.
 
